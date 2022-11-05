0bccbf49a758c0b3055da6634c137530b288c235
o.hollander <o.hollander1990@gmail.com>
2022-11-05 15:47:52 -0400

    Added Data

--- scrapli-xpath.py
+++ scrapli-xpath.py
@@ -1,14 +1,14 @@
-from scrapli_netconf.driver import NetconfScrape
+from scrapli_netconf.driver import NetconfDriver
 
 my_device = {
     "host": "192.168.2.11",
     "auth_username": "okan",
     "auth_password": "hollander",
     "auth_strict_key": False,
-    "port": 830
+    "port": 443
 }
 
-conn = NetconfScrape(**my_device)
+conn = NetconfDriver(**my_device)
 conn.open()
 
 ospf_xpath = '/ospf-oper-data/ospf-state/ospf-instance[af="address-family-ipv4" and router-id="16843009"]/ospf-area[area-id=0]/ospf-interface[name="GigabitEthernet2"]/ospf-neighbor[neighbor-id="2.2.2.2"]/state'
